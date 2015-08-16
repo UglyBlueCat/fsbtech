3b6ed7cfeb28410d144ffa10b8e03421b431d43d
Robin Spinks <uglybluecat@googlemail.com>
2015-08-16 10:39:12 +0100

    Initial Commit

---
+++ fsbtech/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fsbtech
+//
+//  Created by Robin Spinks on 16/08/2015.
+//  Copyright (c) 2015 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fsbtech/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fsbtech
+//
+//  Created by Robin Spinks on 16/08/2015.
+//  Copyright (c) 2015 Robin Spinks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
